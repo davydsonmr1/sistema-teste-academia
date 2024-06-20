cd23e492c6bdbf61c206ede5375d192ea7bbcc31
davydsonmr1 <davydsonmaciel.dev@gmail.com>
2024-06-20 11:33:55 -0300

    v1.2

--- script.js
+++ script.js
@@ -97,6 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
         exibirAlunos();
         inputSeries.value = '';
         inputRepeticoes.value = '';
+        inputExercicio.value = '';
     });
     
 
